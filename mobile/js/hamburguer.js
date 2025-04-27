b0466bcb9622f135828abe712c3a8b0e47fe8929
Anthonny Gabriel <anthonnygabriel50000@gmail.com>
2025-04-27 03:45:34 -0300

    Third commit

--- mobile/js/hamburguer.js
+++
@@ -1,6 +0,0 @@
-const navHamburguer = document.getElementById('nav-hamburguer');
-const navLinks = document.querySelector('.nav-links');
-
-navHamburguer.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
